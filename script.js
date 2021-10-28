6e86d29d8ee09d1c40d14b151dd9b91003e47791
Kip-Hub <daniel.kip@hva.nl>
2021-10-28 15:01:07 +0200

    testing table

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const dataset = '/data/tech-track-dataset.json';
 const windrichtingKey = 'Wat is je favoriete windrichting?';
 const oogkleurKey = 'Wat is je oogkleur?';
+const huisdierKey = 'Wat is je favoriete soort huisdier?';
 
 fetchData(dataset).then((data) => {
     console.log("data fetched");
 
-    const windrichtingen = parseWindrichting(data, windrichtingKey);
-    console.log(windrichtingen);
+    const windRichting = parseWindrichting(data, windrichtingKey);
+    // console.log(windRichting);
 
     const oogKleur = parseOogkleuren(data, oogkleurKey);
-    console.log(oogKleur);
+
+    const huisDier = parseHuisdieren(data, huisdierKey);
 
     let cleanedData = []
 
     for (let i = 0; i < data.length; i++) {
         cleanedData.push({
-            Windrichting: windrichtingen[i],
-            Oogkleur: oogKleur[i]
+            fav_windrichting: windRichting[i],
+            oogkleur: oogKleur[i],
+            fav_huisdier: huisDier[i]
         })
     }
 
     console.table(cleanedData);
+    // showCleanedData(cleanedData);
 
 });
 
@@ -35,12 +39,23 @@ async function fetchData(url) {
 }
 
 function parseWindrichting(arr, key) {
-
     var result = arr.map((item) => (item[key]).toString().toLowerCase());
     return result;
 }
 
 function parseOogkleuren(arr, key) {
-    var result = arr.map((item) => (item[key]).toString().toLowerCase().replace('-', ''));
+    var result = arr.map((item) => (item[key]).toString().toLowerCase().replace('-', ',').replace(' , ', ','));
     return result;
 }
+
+function parseHuisdieren(arr, key) {
+    var result = arr.map((item) => (item[key]).toString().toLowerCase().replace(' of ', ',').replace(' ', '_'));
+    return result;
+}
+
+// function showCleanedData(data) {
+
+//     const container = document.querySelector("section:first-of-type");
+//     const text = document.createElement("table");
+//     container.appendChild(text);
+// }
