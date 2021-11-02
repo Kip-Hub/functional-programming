e42dd82980d62b364f70ee409fe105ff7e5eebd2
Kip-Hub <daniel.kip@hva.nl>
2021-11-02 23:29:49 +0100

    added: some styling to wk1

--- wk1/week1.js
+++ wk1/week1.js
@@ -2,16 +2,17 @@ const dataset = '../data/tech-track-dataset.json';
 const windrichtingKey = 'Wat is je favoriete windrichting?';
 const oogkleurKey = 'Wat is je oogkleur?';
 const huisdierKey = 'Wat is je favoriete soort huisdier?';
+const zuivelKey = 'Wat is je favoriete zuivelproduct?';
 
 fetchData(dataset).then((data) => {
     console.log("data fetched");
 
     const windRichting = parseWindrichting(data, windrichtingKey);
-    // console.log(windRichting);
-
     const oogKleur = parseOogkleuren(data, oogkleurKey);
-
     const huisDier = parseHuisdieren(data, huisdierKey);
+    const zuivel = parseZuivel(data, zuivelKey);
+
+
 
     let cleanedData = []
 
@@ -19,13 +20,15 @@ fetchData(dataset).then((data) => {
         cleanedData.push({
             fav_windrichting: windRichting[i],
             oogkleur: oogKleur[i],
-            fav_huisdier: huisDier[i]
+            fav_huisdier: huisDier[i],
+            fav_zuivel: zuivel[i]
         })
     }
 
     console.table(cleanedData);
 
-    showCleanedData(windRichting);
+    renderWindrichtingen(windRichting);
+    renderZuivel(zuivel);
 });
 
 async function fetchData(url) {
@@ -53,10 +56,25 @@ function parseHuisdieren(arr, key) {
     return result;
 }
 
-function showCleanedData(data) {
+function parseZuivel(arr, key) {
+    var result = arr.map((item) => (item[key]).toString().toLowerCase().replace('!', '').replace(',', '').replaceAll(' ', '_'));
+    return result;
+}
 
-    const container = document.querySelector("body");
+function renderWindrichtingen(data) {
+
+    const container = document.querySelector("section:first-of-type ul");
     for (let i = 0; i < data.length; i++) {
-        container.insertAdjacentHTML('beforeend', `<img src="../resources/${data[i]}.png"></img>`);
+        container.insertAdjacentHTML('beforeend', `<li><img id="${data[i]}"src="../resources/${data[i]}.png"></img></li>`);
+
+        //source: https://www.kindpng.com/imgv/himRobb_green-arrows-png-green-arrow-left-png-transparent/
+    }
+}
+
+function renderZuivel(data) {
+
+    const container = document.querySelector("section:last-of-type ul");
+    for (let i = 0; i < data.length; i++) {
+        container.insertAdjacentHTML('beforeend', `<li id="${data[i]}">${data[i]}</li>`);     
     }
 }
